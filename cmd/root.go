204f2de00d704ed31357d2e15a04e91f80327cce
Amedeus Lyakurwa <amedeus926@gmail.com>
2024-08-23 15:38:00 +0300

    feat: Add func to get all user event from github api

--- cmd/root.go
+++ cmd/root.go
@@ -1,28 +1,32 @@
 package cmd
 
 import (
-    "fmt"
-    "os"
-    "github.com/spf13/cobra"
+	"fmt"
+	"github-activity/internal"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-    Use:   "github-activity",
-    Short: "Fetch and display GitHub user activity",
-    Run: func(cmd *cobra.Command, args []string) {
-        if len(args) < 1 {
-            fmt.Println("Please provide a GitHub username")
-            os.Exit(1)
-        }
-        username := args[0]
-        // Call your function to fetch and display activity here
-        fmt.Println("Fetching activity for user:", username)
-    },
+	Use:   "github-activity",
+	Short: "Fetch and display GitHub user activity",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide a GitHub username")
+			os.Exit(1)
+		}
+		username := args[0]
+		// Call your function to fetch and display activity here
+		fmt.Println("Fetching activity for user:", username)
+		internal.FetchUserEvent(username)
+
+	},
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
--- internal/github.go
+++ internal/github.go
@@ -1,1 +1,25 @@
 package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const apiURL = "https://api.github.com/users/%s/events"
+
+func FetchUserEvent(username string) ([]Event, error) {
+	url := fmt.Sprintf(apiURL, username)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var events []Event
+	if err := json.NewDecoder(resp.Body).Decode(&events); err != nil {
+		return nil, err
+	}
+	fmt.Println(events)
+	return events, nil
+}
